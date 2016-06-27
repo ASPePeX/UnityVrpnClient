97c561458003d35e9823d174f63dff023a57b2ef
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2016-06-27 23:58:21 +0200

    Basic project structure

---
+++ src/UnityVrpnClient.h
@@ -0,0 +1,13 @@
+// UnityVrpnClient.h
+
+#pragma once
+
+using namespace System;
+
+namespace UnityVrpnClient {
+
+	public ref class VrpnClient
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
